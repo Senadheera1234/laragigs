254fecbfd6e98a507a98b4484e6cc786abac4cb8
Senadheera1234 <142715993+Senadheera1234@users.noreply.github.com>
2024-06-20 11:55:50 +0530

    adding tag filter and search filter

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Listing extends Model
 {
     use HasFactory;
+
+    // just by creating this our listing model will be able to filtere
+    public function scopeFilter($query, array $filters){
+
+        // ?? false means if this is  true, go into the if function
+        if($filters['tag'] ?? false){
+
+            // use this to find the tag by ignoring the elements before and after the tag
+            $query->where('tags', 'like', '%'. request('tag'). '%');
+        }
+
+        if($filters['search'] ?? false){
+
+            // here, the search query check in all the fields including title, description, and tags
+            $query->where('title', 'like', '%'. request('search'). 
+            '%')
+            ->orWhere('description', 'like', '%'. request('search'). 
+            '%')
+            ->orWhere('tags', 'like', '%'. request('search'). 
+            '%');
+        }
+    }
 }
