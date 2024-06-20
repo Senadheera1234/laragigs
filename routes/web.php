24fe65fe854ee5930d23f75fad0bc8fc624c0ebc
Senadheera1234 <142715993+Senadheera1234@users.noreply.github.com>
2024-06-20 10:33:59 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,14 @@ use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Listing;
 
+// Common Resource Routes:
+// index - Show all listings
+// show - Show single listing
+// create - Show form to create new listing
+// edit - Show form to edit listing
+// update - Update listing
+// destroy - Delete listing
+
 
 //all Listings
 Route::get('/', [ListingController::class, 'index']);
@@ -14,10 +22,3 @@ Route::get('/listings/{listing}', [ListingController::class, 'show']);
 
 
 
-// Common Resource Routes:
-// index - Show all listings
-// show - Show single listing
-// create - Show form to create new listing
-// edit - Show form to edit listing
-// update - Update listing
-// destroy - Delete listing
